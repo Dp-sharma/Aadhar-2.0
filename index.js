7b50429af3113918203f4013cc8249af1880884f
Deepansh Sharma <deepanshsharma.dp1135@gmail.com>
2024-12-27 19:41:00 +0530

    Registration Backend code

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const path = require('path');
 const routes = require('./routes/userRoutes')
+console.log("I am running ");
 const app = express()
 const port = 3000
 
 // Middleware
+require('dotenv').config();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+mongoose.connect(process.env.MONGODB_URL)
+console.log('Database connected successfully')
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
